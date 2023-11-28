0c0aedf76ff67b66a8f5f662f9436014e61a5140
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-11-28 20:51:30 -0300

    Iniciando o projeto
    Iníciando o projeto

---
+++ costs/src/App.js
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+    </Router>
+  );
+}
+
+export default App;
