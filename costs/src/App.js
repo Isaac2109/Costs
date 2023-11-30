d46117448f927418f512cff34918b90cb90540cc
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-11-29 21:05:56 -0300

    Update Costs
    Estruturando o projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,12 +1,25 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contatc from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/company" element={<Company />}/>
+        <Route path="/contact" element={<Contatc />}/>
+        <Route path="/newproject" element={<NewProject />}/>
+      </Routes>
     </Router>
   );
 }
---
+++ costs/src/components/pages/Contact.js
@@ -0,0 +1,5 @@
+function Contatc(){
+    return <h1>Contato</h1>
+}
+
+export default Contatc
