e887ff4361c4de70fddf57c44277e675b1fcd40d
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-12-06 17:28:30 -0300

    Criando a API do projeto.

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,25 @@
+import {useState} from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btntext}) {
+
+    const [categories, setCategories] = useState([])
+
+    fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'conten-type': 'application/json'
+        },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+    .catch(err => console.log(err))
+
     return (
         <form className={styles.form}>
             <Input
@@ -18,7 +34,11 @@ function ProjectForm({btntext}) {
                 name="budget"
                 placeholder="Insira o orçamento total"
             />
-            <Select name="category_id" text="Selecione a Categoria"/>
+            <Select 
+            name="category_id" 
+            text="Selecione a Categoria"
+            options={categories}
+            />
             <SubmitButton text={btntext}/>
         </form>
     )
